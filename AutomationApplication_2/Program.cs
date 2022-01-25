86df8f99051da7492ad26989a8eb5588126a6c29
Benjamin <shadow_747@yahoo.com>
2022-01-25 12:00:13 -0500

    Actual code

---
+++ AutomationApplication_2/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace program
+{
+    public class Program
+    {
+        public static void Main(string [] args)
+        {
+            Console.WriteLine("Welcome, Benjamin");
+        }
+    }
+}
